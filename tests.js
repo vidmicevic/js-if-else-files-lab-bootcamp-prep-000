b891c55160eba4545de41e609fed9c21758fca61
Vid Micevic <vid.micevic@asu.edu>
2017-12-27 14:32:26 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,14 +2,10 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("Susan")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
-}
+
 
 
 
 if (index.height === 70) {
     console.log("70")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
-}
+
