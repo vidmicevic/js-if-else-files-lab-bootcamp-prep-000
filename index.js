dfc6377f0a688cec0dbdb612a679721e89215523
Vid Micevic <vid.micevic@asu.edu>
2017-12-27 14:33:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ else {
 }
 
 if (height === "70") {
-  console.log("The name is correct")
+  console.log("The height is correct")
 }
 else {
-  console.log ("Expect the name to be Susan")
+  console.log ("Expect the height to be 70")
 }
 // Don't worry about this
 module.exports = { name, height
